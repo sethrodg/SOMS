e29c5e370df16b88d3b836467cae0861c4e2b404
Lawrence475 <Lawrencew009@gmail.com>
2022-02-11 17:55:06 -0600

    updated the login page

--- my-app/src/components/loginpage.js
+++ my-app/src/components/loginpage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./loginpage.css";
 const Login= () =>{
     return (
         <div class="Background">
@@ -6,18 +7,22 @@ const Login= () =>{
                 <div class="login">
                     <h1>Login</h1>
                     <form>
-                        <input type="text" placeholder="Username"></input>
-                        <input type="password" placeholder="Password"></input>
-                        <button type="submit">Login</button>
+                        <div class ="loginform">
+                            <input type="text" placeholder="Username"></input>
+                            <input type="password" placeholder="Password"></input>
+                            <button type="submit">Login</button>
+                        </div>
                     </form>
                 </div>
                 <div class="signup">
                     <h1>Sign Up</h1>
                     <form>
-                        <input type="text" placeholder="Username"></input>
-                        <input type="password" placeholder="Password"></input>
-                        <input type="password" placeholder="Confirm Password"></input>
-                        <button type="submit">Sign Up</button>
+                        <div class ="signupform">
+                            <input type="text" placeholder="Username"></input>
+                            <input type="password" placeholder="Password"></input>
+                            <input type="password" placeholder="Confirm Password"></input>
+                            <button type="submit">Sign Up</button>
+                        </div>
                     </form>
                 </div>
             </div>

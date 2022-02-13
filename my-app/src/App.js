0cfdc0281b9f32b2fe643615ef39377d1d7d917c
sethrodg <sethallenrodgers@gmail.com>
2022-02-12 21:36:55 -0600

    connected database

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,48 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Login from './components/loginpage';
 import MainPage from './components/mainpage';
+
+//Firebase
+import { initializeApp } from 'firebase/app';
+//import { getDatabase } from "firebase/database";
+import { getDatabase, ref, set } from "firebase/database";
+
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/login" element={<Login/>}/>
-        <Route exact path="/home" element={<MainPage/>}/>
-      </Routes>
-    </Router>
-  );
+    return ( <
+        Router >
+        <
+        Routes >
+        <
+        Route exact path = "/login"
+        element = { < Login / > }
+        /> <
+        Route exact path = "/home"
+        element = { < MainPage / > }
+        /> < /
+        Routes > <
+        /Router>
+    );
 }
+
+
+//Configure Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDdSTvLk9KzL3GkBSFOoTX7DS9PaqqIqaU",
+    authDomain: "soms-409ff.firebaseapp.com",
+    databaseURL: "https://soms-409ff-default-rtdb.firebaseio.com",
+};
+const app = initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+const db = getDatabase(app);
+
+function writeUserData(userId, name, email) {
+    set(ref(db, 'users/' + userId), {
+        username: name,
+        email: email
+    });
+}
+writeUserData("sar2402", "Seth", "sethallenrodgers@gmail.com")
+
 
 export default App;

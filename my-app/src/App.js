71e0db1b000def599814339396c0ad01f9acd47e
Lawrence475 <Lawrencew009@gmail.com>
2022-02-19 14:03:39 -0600

    still trying to adjust signup page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,11 +6,6 @@ import Login from './components/loginpage';
 import MainPage from './components/mainpage';
 
 
-//Firebase
-import { initializeApp } from 'firebase/app';
-//import { getDatabase } from "firebase/database";
-import { getDatabase, ref, set } from "firebase/database";
-
 const App = () => {
   return (
     <Router>
@@ -22,26 +17,5 @@ const App = () => {
     </Router>
   );
 }
-
-
-//Configure Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDdSTvLk9KzL3GkBSFOoTX7DS9PaqqIqaU",
-  authDomain: "soms-409ff.firebaseapp.com",
-  databaseURL: "https://soms-409ff-default-rtdb.firebaseio.com",
-};
-const app = initializeApp(firebaseConfig);
-
-// Get a reference to the database service
-const db = getDatabase(app);
-
-function writeUserData(userId, name, email) {
-  set(ref(db, 'users/' + userId), {
-    username: name,
-    email: email
-  });
-}
-writeUserData("sar2402", "Seth", "sethallenrodgers@gmail.com")
-
 
 export default App;

348653040700c430f055b8ab91344b10b4688d42
Lawrence475 <Lawrencew009@gmail.com>
2022-03-28 19:06:13 -0500

    fixing the main branch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './navbar/navbar';
 import Login from './components/loginpage';
-import MainPage from './components/mainpage';
 import SignUp from './components/signup';
-import Main from './components/main.js';
+import Main from './components/main';
 import Explore from './components/explore';
 const App = () => {
   return (

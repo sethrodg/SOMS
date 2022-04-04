400f1783bbbf6963998ee8a3d71973284b6a2c39
Lawrence475 <Lawrencew009@gmail.com>
2022-04-04 15:09:45 -0500

    able to add the items from database to the carousel, still need refining

--- my-app/src/components/explore.js
+++ my-app/src/components/explore.js
@@ -6,17 +6,35 @@ import { db } from '../firebase';
 import "./explore.css"
 const Explore = () => {
     let systemnames = [];
+    let select = document.getElementsByClassName("gallery__item"); //Will appear twice in the dropdown
     let systemlead = [];
     let systemdesc = [];
+    function arrayRemove(arr, value) { 
+    
+        return arr.filter(function(ele){ 
+            return ele != value; 
+        });
+    }
     const s = query(collection(db, "Systems"))
     const unsubb = onSnapshot(s, (querySnapshot) => {
         const response = querySnapshot.docs.map(doc => doc.data());
         response.forEach(element => {
-            systemnames.push(element.name);
-            systemlead.push(element.SystemLead);
+            var opt = element.name;
+            systemnames.push(opt);
         });
+        
+        let list = document.getElementById("carouselList");
+        systemnames.forEach(item => {
+        console.log(item);
+        let li = document.createElement("li");
+        li.innerText = item;
+        list.appendChild(li);
+        });
+        console.log(list);
     });
-    console.log(systemnames);
+
+
+
     document.addEventListener('DOMContentLoaded', function() {
         var stream = document.querySelector('.gallery__stream');
         var items = document.querySelectorAll('.gallery__item');
@@ -33,35 +51,15 @@ const Explore = () => {
           items = document.querySelectorAll('.gallery__item');
         });
       });
-    // Can't add list elements since line 57 doesn't work
-    //   function makeUL(array) {
-    //     // Create the list element:
-    //     var list = document.createElement('ul');
+    //Can't add list elements since line 57 doesn't work
     
-    //     for(var i = 0; i < array.length; i++) {
-    //         // Create the list item:
-    //         var item = document.createElement('li');
-    
-    //         // Set its contents:
-    //         item.appendChild(document.createTextNode(array[i]));
-    
-    //         // Add it to the list:
-    //         list.appendChild(item);
-    //     }
-    
-    //     // Finally, return the constructed list:
-    //     return list;
-    // }
-    
-    // Add the contents of options[0] to #foo:
-    //document.getElementById('carouselList').appendChild(makeUL(systemnames));
     return (
         <div className = "testing">
-            <div id="carouselList"/>
             <div class="gallery">
-            <div class="gallery__prev"></div>
-            <div class="gallery__next"></div>
-                <div class="gallery__stream">
+                <div class="gallery__prev"></div>
+                <div class="gallery__next"></div>
+                <div class ="gallery__stream">
+                    <div class = "gallery__item" id = "carouselList"/>
                     <div class="gallery__item bg-1">ONE</div>
                     <div class="gallery__item bg-2">TWO</div>
                     <div class="gallery__item bg-3">THREE</div>

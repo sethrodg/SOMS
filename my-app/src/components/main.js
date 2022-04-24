94bcbe5f42db8b125ca4a992cef34a4ff1fbcd83
Lawrence475 <Lawrencew009@gmail.com>
2022-04-24 13:31:19 -0500

    removed code for horizontal carousel for the mainpage

--- my-app/src/components/main.js
+++ my-app/src/components/main.js
@@ -136,64 +136,7 @@ function Pop() {
         var Welcome = "Welcome Guest";
         var email = ""; //setting the email to be blank as there's no current user
     }
-    // var counter = 0;
-    // joblist.forEach(item => {
-    //     let parentnode = document.getElementById("div1"); //make the parent node
-    //     let child = document.createElement("div"); //make the child
-    //     child.classList = "gallery__item"; //give the child the class (gallery__item) is what's used in CSS to make it all work across the board
-    //     child.id = "gallery__item" + counter; //needed to create an id so that we're able to target the specific job when we click on it.
-    //     let jobtitle = document.createElement("div"); //giving the child some content
-    //     jobtitle.classList = "JobTitle"; //making a class for the title of the jobtitles
-    //     let titletext = document.createTextNode(item);
-    //     jobtitle.appendChild(titletext); //appending the text to a div
-    //     let systemdesc = document.createElement("div"); //making a div for the description for the systems
-    //     systemdesc.classList = "Job_Description";
-    //     let desctext = document.createTextNode(jobinfo[counter]);
-    //     systemdesc.appendChild(desctext); //appending the text to a div
-    //     let Jsystem = document.createElement("div");
-    //     Jsystem.classList = "Job_System";
-    //     let systemtext = document.createTextNode(jobsystem[counter]);
-    //     let enrollBtn = document.createElement("button");
-    //     Jsystem.appendChild(systemtext);
-    //     let duedate = document.createElement("div");
-    //     duedate.classList = "Job_Duedate";
-    //     let duedatetext = document.createTextNode(jobdate[counter]);
-    //     duedate.appendChild(duedatetext);
-    //     enrollBtn.classList = "enrollBtn";
-    //     enrollBtn.innerHTML = "Enroll";
 
-    //     child.appendChild(Jsystem);
-    //     child.appendChild(jobtitle);
-    //     child.appendChild(systemdesc);
-    //     child.appendChild(duedate);
-    //     enrollBtn.addEventListener("click", function (){
-    //         currentJobName = item;
-    //         console.log(currentJobName);
-    //         currentJobSystem = document.getElementById(child.id).getElementsByClassName("Job_Description")[0].innerHTML;
-    //         console.log(currentJobSystem); //this doesn't work. it says that there's no user that's currently logged in (this might be an async issue with javascript)
-    //         //getinfo(user, currentJobName, currentJobSystem);
-    //     });
-    //     //enrollBtn.addEventListener("click", UpdateP); //item, document.getElementById(child.id).getElementsByClassName("Job_Description")[0].innerHTML )}); 
-    //     child.append(enrollBtn);
-    //     parentnode.appendChild(child); // finally appending all the children nodes holding the content to the parent carousel card.
-    //     counter += 1; //incrementing counter to ensure we pair the correct description + image with the system
-    // });
-    // document.addEventListener('DOMContentLoaded', function () { //this is for the carousel
-    //     var stream = document.querySelector('.gallery__stream');
-    //     var items = document.querySelectorAll('.gallery__item');
-
-    //     var prev = document.querySelector('.gallery__prev');
-    //     prev.addEventListener('click', function () {
-    //         stream.insertBefore(items[items.length - 1], items[0]);
-    //         items = document.querySelectorAll('.gallery__item');
-    //     });
-
-    //     var next = document.querySelector('.gallery__next');
-    //     next.addEventListener('click', function () {
-    //         stream.appendChild(items[0]);
-    //         items = document.querySelectorAll('.gallery__item');
-    //     });
-    // });
     //if the user is an admin then we'll have to hide the system creation as well as the job creation
     const testingtasking = document.getElementsByClassName("Tasking");
     const testingjobs = document.getElementsByClassName("Jobs");
